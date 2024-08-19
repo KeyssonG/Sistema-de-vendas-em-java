e2668a0c36daaa126321a75457bd0eb61b6075ef
keyssonG <104814645+KeyssonG@users.noreply.github.com>
2024-08-19 06:51:26 -0300

    Criação de controler de rota para teste de execução de arquivo html com sucesso.

---
+++ com/projeto/sistema/SistemaApplication.java
@@ -0,0 +1,10 @@
+package com.projeto.sistema;
+
+@SpringBootApplication
+public class SistemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaApplication.class, args);
+	}
+
+}
---
+++ sistema/src/main/java/com/projeto/sistema/SistemaAplication.java
@@ -0,0 +1,12 @@
+package com.projeto.sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SistemaAplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaAplication.class, args);
+    }
+}
