498d6599905431b885e671abc80e24677b035315
keyssonG <104814645+KeyssonG@users.noreply.github.com>
2024-08-21 02:08:56 -0300

    Criando modelo-Estado para mapeamento da tabela no banco de dados. Criado Estado repositorio para manipulação e consultado de dados no banco. EstadoControle em desenvolvimento.

---
+++ sistema/src/main/java/com/projeto/sistema/controle/EstadoControle.java
@@ -0,0 +1,10 @@
+package com.projeto.sistema.controle;
+import com.projeto.sistema.repositorios.EstadoRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EstadoControle {
+    @Autowired
+    private EstadoRepositorio estadoRepositorio
+}
