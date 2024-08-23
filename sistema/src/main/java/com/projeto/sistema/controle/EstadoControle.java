8c85cacc7be17e96aa52990d5a6fa5eba28c7da1
keyssonG <104814645+KeyssonG@users.noreply.github.com>
2024-08-23 12:21:38 -0300

    Mapeado rota /cadastroEstado para o método cadastrar. O método cria e retorna visualização no caminho administrativo/estados/cadastro. Onde está incluido o objeto estado passado como parametro.

--- sistema/src/main/java/com/projeto/sistema/controle/EstadoControle.java
+++ sistema/src/main/java/com/projeto/sistema/controle/EstadoControle.java
@@ -1,10 +1,20 @@
 package com.projeto.sistema.controle;
+import com.projeto.sistema.modelos.Estado;
 import com.projeto.sistema.repositorios.EstadoRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class EstadoControle {
     @Autowired
-    private EstadoRepositorio estadoRepositorio
+    private EstadoRepositorio estadoRepositorio;
+
+    @GetMapping("/cadastroEstado")
+    public ModelAndView cadastrar(Estado estado) {
+        ModelAndView mv = new ModelAndView("administrativo/estados/cadastro");
+        mv.addObject("estado", estado);
+        return mv;
+    }
 }
