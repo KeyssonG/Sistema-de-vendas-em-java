28ba26e6c68175233c866ad630e17253eee90bca
keyssonG <104814645+KeyssonG@users.noreply.github.com>
2024-08-23 17:28:16 -0300

    Criando o metodo salvar com a rota /salvarEstado, foi utilizando o BindingResult para verificar erros, e caso caia em hasErros, ele retorna para cadastrar estado, caso não ocorra erros, chama o estadoRepositorio, onde passa a entidade e id, e retorna cadastrar novo estado.

--- sistema/src/main/java/com/projeto/sistema/controle/EstadoControle.java
+++ sistema/src/main/java/com/projeto/sistema/controle/EstadoControle.java
@@ -3,7 +3,9 @@ import com.projeto.sistema.modelos.Estado;
 import com.projeto.sistema.repositorios.EstadoRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -16,5 +18,13 @@ public class EstadoControle {
         ModelAndView mv = new ModelAndView("administrativo/estados/cadastro");
         mv.addObject("estado", estado);
         return mv;
+    }
+    @PostMapping("/salvarEstado")
+    public ModelAndView salvar(Estado estado, BindingResult result){
+        if(result.hasErrors()) {
+            return cadastrar(estado);
+        }
+        estadoRepositorio.saveAndFlush(estado);
+        return cadastrar(new Estado());
     }
 }
