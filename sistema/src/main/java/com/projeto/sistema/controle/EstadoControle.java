47127fc60d6199053dcc9b8b65242d383616e8e0
keyssonG <104814645+KeyssonG@users.noreply.github.com>
2024-08-31 12:15:19 -0300

    Criado método DELETE para remover o item do objeto estado. {id} define o caminho do endpoint, parametro que será substituido pelo valor fornecido na URL. Após a exclusão, o método retorna o resultado do método listar.

--- sistema/src/main/java/com/projeto/sistema/controle/EstadoControle.java
+++ sistema/src/main/java/com/projeto/sistema/controle/EstadoControle.java
@@ -5,6 +5,7 @@ import com.projeto.sistema.repositorios.EstadoRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,6 +36,13 @@ public class EstadoControle {
     public ModelAndView editar(@PathVariable("id") Long id) {
         Optional<Estado> estado = estadoRepositorio.findById(id);
         return cadastrar(estado.get());
+    }
+
+    @DeleteMapping("/removerEstado/{id}")
+    public ModelAndView remover(@PathVariable("id") Long id) {
+        Optional<Estado> estado = estadoRepositorio.findById(id);
+        estadoRepositorio.delete(estado.get());
+        return listar();
     }
 
     @PostMapping("/salvarEstado")
