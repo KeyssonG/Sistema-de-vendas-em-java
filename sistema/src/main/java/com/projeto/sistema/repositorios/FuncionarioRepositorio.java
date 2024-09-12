641033f8c48ca92625a255f0fc182736817792cb
keyssonG <104814645+KeyssonG@users.noreply.github.com>
2024-09-12 18:17:53 -0300

    Criado módulo de funcionários incluido cadastro e listagem.

---
+++ sistema/src/main/java/com/projeto/sistema/repositorios/FuncionarioRepositorio.java
@@ -0,0 +1,6 @@
+package com.projeto.sistema.repositorios;
+import com.projeto.sistema.modelos.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FuncionarioRepositorio extends JpaRepository<Funcionario, Long> {
+}
