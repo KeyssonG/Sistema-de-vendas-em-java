78c1dac43f7f12bac528aafce405074717f3232a
keyssonG <104814645+KeyssonG@users.noreply.github.com>
2024-09-17 10:09:35 -0300

    Criado módulo de clientes tanto para cadastro e listagem, parte de back-end e front testado e funcionando.

---
+++ sistema/src/main/java/com/projeto/sistema/repositorios/ClienteRepositorio.java
@@ -0,0 +1,6 @@
+package com.projeto.sistema.repositorios;
+import com.projeto.sistema.modelos.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepositorio extends JpaRepository<Cliente, Long> {
+}
