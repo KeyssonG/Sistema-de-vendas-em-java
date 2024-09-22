2fcc6039ce2db5dac2373cab01ccdff5e4c7f0be
keyssonG <104814645+KeyssonG@users.noreply.github.com>
2024-09-21 23:08:39 -0300

    Em desenvolvimento módulo de entrada de produto. No momento, foi feito parte do Front-end com um formulário que será alterado. Back-end em desenvolvimento, irá ocorrer ajustes.

---
+++ sistema/src/main/java/com/projeto/sistema/modelos/ItemEntrada.java
@@ -0,0 +1,90 @@
+package com.projeto.sistema.modelos;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+public class Entrada implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String nome;
+    private String obs;
+    private double valorTotal = 0.00;
+    private double quantidadeValor = 0.00;
+    private Date dataEntrada = new Date();
+    private Fornecedor fornecedor;
+
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
+
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    public Date getDataEntrada() {
+        return dataEntrada;
+    }
+
+    public void setDataEntrada(Date dataEntrada) {
+        this.dataEntrada = dataEntrada;
+    }
+
+    public double getQuantidadeValor() {
+        return quantidadeValor;
+    }
+
+    public void setQuantidadeValor(double quantidadeValor) {
+        this.quantidadeValor = quantidadeValor;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public String getObs() {
+        return obs;
+    }
+
+    public void setObs(String obs) {
+        this.obs = obs;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    private Funcionario funcionario;
+
+}
